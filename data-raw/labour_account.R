5d2f80b8c369ed4da6fef57b1569a4d6985049f1
Hamish Gamble <gamb0043@flinders.edu.au>
2020-09-10 09:43:40 +0930

    remove condition

--- data-raw/labour_account.R
+++ data-raw/labour_account.R
@@ -5,30 +5,27 @@ library(dplyr)
 library(lubridate)
 
 
-if (!abs_data_up_to_date("6150.0.55.003")) {
-  message("updating `data/labour_account.rda`")
-  download_abs_data_cube('6150.0.55.003', cube = '6150055003do001_2019202006', path = here::here("data-raw"))
-  
-  file.rename(here::here("data-raw","6150055003do001_2019202006.xls"), here::here("data-raw","labour_account.xls"))
+message("updating `data/labour_account.rda`")
+download_abs_data_cube('6150.0.55.003', cube = '6150055003do001_2019202006', path = here::here("data-raw"))
 
-  raw <- read_abs_local(path = here::here("data-raw"), filenames = "labour_account.xls")
+file.rename(here::here("data-raw","6150055003do001_2019202006.xls"), here::here("data-raw","labour_account.xls"))
 
-  labour_account <- raw %>%
-    separate(series,
-             into = c("prefix", "indicator", "state", "industry"),
-             sep = ";",
-             extra = "drop") %>%
-    mutate_if(is.character, trimws)  %>%
-    filter(!is.na(value)) %>%
-    mutate(year = year(date),
-           month = month(date, abbr = FALSE, label = TRUE)) %>%
-    select(date, month, year, prefix, indicator, state, industry, series_type, value, unit)
-  
-  file.remove(here::here("data-raw", "labour_account.xls"))
-  
-  save(labour_account, file = here::here("data", "labour_account.rda"), compress = "xz")
-  
-} else {
-  message("`data/labour_account.rda` is already up to date")
-}
+raw <- read_abs_local(path = here::here("data-raw"), filenames = "labour_account.xls")
+
+labour_account <- raw %>%
+  separate(series,
+           into = c("prefix", "indicator", "state", "industry"),
+           sep = ";",
+           extra = "drop") %>%
+  mutate_if(is.character, trimws)  %>%
+  filter(!is.na(value)) %>%
+  mutate(year = year(date),
+         month = month(date, abbr = FALSE, label = TRUE)) %>%
+  select(date, month, year, prefix, indicator, state, industry, series_type, value, unit)
+
+file.remove(here::here("data-raw", "labour_account.xls"))
+
+save(labour_account, file = here::here("data", "labour_account.rda"), compress = "xz")
+
+
 

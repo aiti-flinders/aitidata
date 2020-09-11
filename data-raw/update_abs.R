8f5b865740be29652d772d06ef5aa1df1226104a
Hamish Gamble <gamb0043@flinders.edu.au>
2020-09-11 09:46:23 +0930

    add payroll

--- data-raw/update_abs.R
+++ data-raw/update_abs.R
@@ -7,3 +7,10 @@ if (!daitir::abs_data_up_to_date("6150.0.55.003")) {
   source(here::here("data-raw", "labour_account.R"))
 }
 
+if (!daitir::abs_data_up_to_date("6160.0.55.001", "payroll_index")) {
+  source(here::here("data-raw", "payroll_index.R"))
+}
+
+if (!daitir::abs_data_up_to_date("6160.0.55.001", "payroll_sa4")) {
+  source(here::here("data-raw", "payroll_sa4.R"))
+}

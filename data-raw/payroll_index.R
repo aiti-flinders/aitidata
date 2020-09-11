216f57579d3cd8484709d71608d0ff16d1e9d698
Hamish Gamble <gamb0043@flinders.edu.au>
2020-09-11 13:19:58 +0930

    trying to get conditions to work

--- data-raw/payroll_index.R
+++ data-raw/payroll_index.R
@@ -1,30 +1,32 @@
 ## code to prepare `payroll_index` dataset goes here
-library(readabs)
-library(tidyr)
-library(stringr)
-library(forcats)
-library(dplyr)
-library(readxl)
 
-download_abs_data_cube("6160.0.55.001", cube = "6160055001_do004.xlsx", path = here::here("data-raw"))
-file.rename(here::here("data-raw", "6160055001_do004.xlsx"), here::here("data-raw", "payroll_index.xlsx"))
-
-payroll_index <- read_xlsx(here::here("data-raw", "payroll_index.xlsx"), sheet = "Payroll jobs index", skip = 5, na = "NA", n_max = 4321) %>%
-  janitor::clean_names() %>%
-  mutate(across(starts_with("x"), as.numeric)) %>%
-  pivot_longer(cols = c(5:length(.)),
-               names_to = "date",
-               values_to = "value") %>%
-  mutate(across(1:5, ~str_remove_all(., "^[0-9]\\. ")),
-         across(industry_division, ~str_remove_all(., "(0[0-9]|1[0-9])\\. ([A-S]-)")),
-         across(date, ~str_remove_all(., "x") %>% as.numeric() %>% as.Date(., origin = "1899-12-30")),
-         across(value, ~as.numeric(.)),
-         across(age_group, ~as_factor(.)),
-         across(state_or_territory, ~strayr::strayr(., to = "state_name")),
-         across(industry_division, ~str_to_title(.) %>% str_replace_all(., "&", "and"))) %>%
-  select(date, gender = sex, age = age_group, state = state_or_territory, industry = industry_division, value)
-
-file.remove(here::here("data-raw", "payroll_index.xlsx"))
-
-save(payroll_index, file = here::here("data", "payroll_index.rda"), compress = "xz")
-         
+if(!daitir::abs_data_up_to_date('6160.0.55.001', 'payroll_index')) {
+  library(readabs)
+  library(tidyr)
+  library(stringr)
+  library(forcats)
+  library(dplyr)
+  library(readxl)
+  
+  download_abs_data_cube("6160.0.55.001", cube = "6160055001_do004.xlsx", path = here::here("data-raw"))
+  file.rename(here::here("data-raw", "6160055001_do004.xlsx"), here::here("data-raw", "payroll_index.xlsx"))
+  
+  payroll_index <- read_xlsx(here::here("data-raw", "payroll_index.xlsx"), sheet = "Payroll jobs index", skip = 5, na = "NA", n_max = 4321) %>%
+    janitor::clean_names() %>%
+    mutate(across(starts_with("x"), as.numeric)) %>%
+    pivot_longer(cols = c(5:length(.)),
+                 names_to = "date",
+                 values_to = "value") %>%
+    mutate(across(1:5, ~str_remove_all(., "^[0-9]\\. ")),
+           across(industry_division, ~str_remove_all(., "(0[0-9]|1[0-9])\\. ([A-S]-)")),
+           across(date, ~str_remove_all(., "x") %>% as.numeric() %>% as.Date(., origin = "1899-12-30")),
+           across(value, ~as.numeric(.)),
+           across(age_group, ~as_factor(.)),
+           across(state_or_territory, ~strayr::strayr(., to = "state_name")),
+           across(industry_division, ~str_to_title(.) %>% str_replace_all(., "&", "and"))) %>%
+    select(date, gender = sex, age = age_group, state = state_or_territory, industry = industry_division, value)
+  
+  file.remove(here::here("data-raw", "payroll_index.xlsx"))
+  
+  save(payroll_index, file = here::here("data", "payroll_index.rda"), compress = "xz")
+}
--- data-raw/payroll_sa4.R
+++ data-raw/payroll_sa4.R
@@ -1,62 +1,64 @@
-## code to prepare `payroll_sa4` dataset goes here
-library(dplyr)
-library(tidyr)
-library(janitor)
-library(readxl)
-library(sf)
-library(absmapsdata)
-
 #Payroll SA4 data comes from 6160.0.55.001, table 5 - called "6160055001_do005"
 
-readabs::download_abs_data_cube("6160.0.55.001", cube = "6160055001_do005", path = here::here("data-raw"))
-file.rename(here::here("data-raw", "6160055001_do005.xlsx"), here::here("data-raw", "pyroll_sa4.xlsx"))
-
-payroll_sa4 <- read_xlsx(here::here("data-raw", "payroll_sa4.xlsx"), sheet = "Payroll jobs index-SA4", skip = 5, na = "NA") %>%
-  janitor::clean_names() %>%
-  mutate(across(starts_with("x"), as.numeric)) %>%
-  pivot_longer(cols = c(5:length(.)),
-               names_to = "date",
-               values_to = "value") %>%
-  mutate(across(1:4, ~str_remove_all(., "^[0-9]. ")),
-         across(date, ~str_remove_all(., "x") %>% as.numeric() %>% as.Date(., origin = "1899-12-30")),
-         across(value, ~as.numeric(.)),
-         across(age_group, ~as_factor(.)),
-         sa4_code_2016 = ifelse(statistical_area_4 != "All SA4", str_sub(statistical_area_4, start = 1L, end = 3L), NA)) %>%
-  select(state_name_2016 = state_or_territory,
-         gender = sex,
-         age = age_group,
-         date,
-         value,
-         sa4_code_2016) %>% 
-  mutate(across(state_name_2016, ~strayr::strayr(., to = "state_name")),
-         indicator = "payroll_index") %>%
-  select(-gender, -age) %>%
-  filter(!is.na(sa4_code_2016),
-         !is.na(value))
-
-payroll_industry <- read_xlsx(here::here("data-raw", "payroll_sa4.xlsx"), sheet = "Payroll jobs index-Subdivision", skip = 5, na = "NA", n_max = 1041) %>%
-  janitor::clean_names() %>%
-  pivot_longer(cols = c(5:length(.)),
-               names_to = 'date',
-               values_to = "value") %>%
-  mutate(across(1:4, ~str_remove_all(., "^[0-9]. ")),
-         across(date, ~str_remove_all(., "x") %>% as.numeric() %>% as.Date(., origin = "1899-12-30")),
-         across(value, ~as.numeric(.)),
-         across(age_group, ~as_factor(.)),
-         across(industry, ~str_sub(., 7, str_length(.)))) %>%
-  filter(sex == "Persons", 
-         age_group == "All ages",
-         sub_division == "All sub-divisions",
-         industry != "dustries", 
-         !is.na(value)) %>%
-  filter(date == max(.$date)) %>%
-  select(industry, value) %>%
-  mutate(across(industry, ~str_to_title(.) %>% str_replace_all(., "&", "and"))) 
-
-file.remove(here::here("data-raw", "payroll_sa4.xlsx"))
-
-save(payroll_industry, file = here::here("data", "payroll_industry.rda"), compress = "xz")
-
-save(payroll_sa4, file = here::here("data", "payroll_sa4.rda"), compress = "xz")
+if(!daitir::abs_data_up_to_date("6160.0.55.001", "payroll_sa4")) {
+  ## code to prepare `payroll_sa4` dataset goes here
+  library(dplyr)
+  library(tidyr)
+  library(janitor)
+  library(readxl)
+  library(sf)
+  library(absmapsdata)
+  
+  readabs::download_abs_data_cube("6160.0.55.001", cube = "6160055001_do005", path = here::here("data-raw"))
+  file.rename(here::here("data-raw", "6160055001_do005.xlsx"), here::here("data-raw", "payroll_sa4.xlsx"))
+  
+  payroll_sa4 <- read_xlsx(here::here("data-raw", "payroll_sa4.xlsx"), sheet = "Payroll jobs index-SA4", skip = 5, na = "NA") %>%
+    janitor::clean_names() %>%
+    mutate(across(starts_with("x"), as.numeric)) %>%
+    pivot_longer(cols = c(5:length(.)),
+                 names_to = "date",
+                 values_to = "value") %>%
+    mutate(across(1:4, ~str_remove_all(., "^[0-9]. ")),
+           across(date, ~str_remove_all(., "x") %>% as.numeric() %>% as.Date(., origin = "1899-12-30")),
+           across(value, ~as.numeric(.)),
+           across(age_group, ~as_factor(.)),
+           sa4_code_2016 = ifelse(statistical_area_4 != "All SA4", str_sub(statistical_area_4, start = 1L, end = 3L), NA)) %>%
+    select(state_name_2016 = state_or_territory,
+           gender = sex,
+           age = age_group,
+           date,
+           value,
+           sa4_code_2016) %>% 
+    mutate(across(state_name_2016, ~strayr::strayr(., to = "state_name")),
+           indicator = "payroll_index") %>%
+    select(-gender, -age) %>%
+    filter(!is.na(sa4_code_2016),
+           !is.na(value))
+  
+  payroll_industry <- read_xlsx(here::here("data-raw", "payroll_sa4.xlsx"), sheet = "Payroll jobs index-Subdivision", skip = 5, na = "NA", n_max = 1041) %>%
+    janitor::clean_names() %>%
+    pivot_longer(cols = c(5:length(.)),
+                 names_to = 'date',
+                 values_to = "value") %>%
+    mutate(across(1:4, ~str_remove_all(., "^[0-9]. ")),
+           across(date, ~str_remove_all(., "x") %>% as.numeric() %>% as.Date(., origin = "1899-12-30")),
+           across(value, ~as.numeric(.)),
+           across(age_group, ~as_factor(.)),
+           across(industry, ~str_sub(., 7, str_length(.)))) %>%
+    filter(sex == "Persons", 
+           age_group == "All ages",
+           sub_division == "All sub-divisions",
+           industry != "dustries", 
+           !is.na(value)) %>%
+    filter(date == max(.$date)) %>%
+    select(industry, value) %>%
+    mutate(across(industry, ~str_to_title(.) %>% str_replace_all(., "&", "and"))) 
+  
+  file.remove(here::here("data-raw", "payroll_sa4.xlsx"))
+  
+  save(payroll_industry, file = here::here("data", "payroll_industry.rda"), compress = "xz")
+  
+  save(payroll_sa4, file = here::here("data", "payroll_sa4.rda"), compress = "xz")
+}
 
 

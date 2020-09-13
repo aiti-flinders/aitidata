cddeb5e4ea8551a482fc149506774319bedbc373
Hamish Gamble <hamish.gamble@gmail.com>
2020-09-14 09:16:15 +0930

    use lubridate for tz

--- data-raw/update_abs.R
+++ data-raw/update_abs.R
@@ -1,7 +1,7 @@
 ## code to prepare `update_abs` dataset goes here
-to_update <- unique(abs_cats[abs_cats$next_release == Sys.Date(), ]$data_name)
+to_update <- unique(abs_cats[abs_cats$next_release == lubridate::today(tzone = "Australia/Adelaide"), ]$data_name)
 
-if (length(to_update)) {
+if (length(to_update) == 0) {
   message("everything is up to date!")
 } else {
   file_paths <- purrr::map(to_update, ~here::here("data-raw", paste0(., ".R")))

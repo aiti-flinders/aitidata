933e96ef80d0f40ad03d6d9cd1e87094e80b6766
Hamish Gamble <gamb0043@flinders.edu.au>
2020-10-07 09:18:21 +1030

    clean up

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
 #' Australian and New Zealand Standard Industrial Classification (ANZSIC)
-#' 
+#'
 #' A dataset containing the industry divisions, subdivisions, groups, and classes of the
-#' 2006 ANZSIC organised heirarchically 
-#' 
+#' 2006 ANZSIC organised heirarchically
+#'
 #' @format A dataframe with 506 rows and 4 variables:
 #' \describe{
 #' \item{division}{industry division}
@@ -10,16 +10,16 @@
 #' \item{group}{group of subdivision}
 #' \item{class}{class of group}
 #' }
-#' 
+#'
 #' @source \url{https://www.abs.gov.au/AUSSTATS/abs@.nsf/Lookup/1292.0.55.002Main+Features12006?OpenDocument}
-#' 
+#'
 "anzsic"
 
 #' Counts of Australian Businesses, including Entries and Exists, June 2015 to June 2019
-#' 
+#'
 #' A dataset containing the number of businesses, by industry division, by SA2 statistical areas,
 #' by employment size ranges
-#' 
+#'
 #' @format A dataframe with 6 variables:
 #' \describe{
 #' \item{date}{date}
@@ -28,19 +28,19 @@
 #' \item{sa2_name_2016}{Name of statistical area}
 #' \item{indicator}{Employment ranges}
 #' }
-#' 
+#'
 #' @source \url{https://www.abs.gov.au/AUSSTATS/abs@.nsf/Lookup/8165.0Main+Features2June%202015%20to%20June%202019?OpenDocument}
-#' 
+#'
 "cabee_sa2"
 
 #' COVID-19 Geographic data
-#' 
+#'
 #' A dataset containing the jobkeeper applications, jobseeker payments, and derived impact from COVID-19 at
 #' a geographical level. Jobkeeper and Jobseeker data is based on the SA2 classiciation, and employment
 #' impact is based on the SA4 classification. This dataset combines data from jobseeker_sa2, jobkeeper_sa2,
 #' payroll_sa4, and small_area_labour_market datasets and is intended for use on the AITI Economic
-#' Indicators dashboard. 
-#' 
+#' Indicators dashboard.
+#'
 #' @format A dataframe with 6 variables:
 #' \describe{
 #' \item{date}{Date}
@@ -50,14 +50,14 @@
 #' \item{statistical_area}{Number of digits in statistical area}
 #' \item{statistical_area_code}{Statistical area code}
 #' }
-#' 
+#'
 "covid_data"
 
 #' Australian Subnational Economic Complexity
-#' 
+#'
 #' A dataset containing economic complexity indicators
 #' for Australian states and territories
-#' 
+#'
 #' @format A dataframe with 704 observations and 4 variables
 #' \describe{
 #' \item{location_code}{state or territory abbreviation}
@@ -65,16 +65,16 @@
 #' \item{indicator}{Place specific economic complexity indicator (diversity, eci, coi, total exports)}
 #' \item{value}{Value of indicator}
 #' }
-#' 
+#'
 "economic_complexity"
 
-#'ABS Employment by Industry
+#' ABS Employment by Industry
 #'
-#'A dataset containing quarterly national and sub-national employment indicators
-#'by industry of employment since February 1991. 
+#' A dataset containing quarterly national and sub-national employment indicators
+#' by industry of employment since February 1991.
 #'
-#'@format A dataframe with 11 variables:
-#'\describe{
+#' @format A dataframe with 11 variables:
+#' \describe{
 #' \item{date}{Date}
 #' \item{year}{Year}
 #' \item{month}{Month}
@@ -94,16 +94,16 @@
 #' \item{value}{Value of employment indicator}
 #' \item{unit}{Unit of measurement for indiciator}
 #' }
-#' 
+#'
 #' @source \url{https://www.abs.gov.au/ausstats/abs@.nsf/PrimaryMainFeatures/6291.0.55.001?OpenDocument}
 "employment_by_industry"
 
 #' Internet Vacancies Index
-#' 
+#'
 #' A dataset containing monthly nation and sub-national internet vacancies by
-#' occupation since January 2006. Data is updated monthly, about one week 
-#' after the Labour Force Survey is released. 
-#' 
+#' occupation since January 2006. Data is updated monthly, about one week
+#' after the Labour Force Survey is released.
+#'
 #' @format A dataframe with 7 variables
 #' \describe{
 #' \item{state}{State}
@@ -114,16 +114,16 @@
 #' \item{anzsco_1}{1 digit ANZSCO}
 #' \item{occupation_group}{Occupation group name}
 #' }
-#' 
+#'
 #' @source \url{https://lmip.gov.au/default.aspx?LMIP/GainInsights/VacancyReport}
 "internet_vacancies_index"
 
 #' JobKeeper data by SA2
-#' 
+#'
 #' This dataset uses treasury estimates of JobKeeper applications by
 #' postcode, and reconciles that to a statistical area measure instead.
 #' Conversion is done through apportioning businesses via meshblocks.
-#' 
+#'
 #' @format A dataframe with 4 variables
 #' \describe{
 #' \item{sa2_main_2016}{Statistical Area 2 Code}
@@ -131,16 +131,16 @@
 #' \item{indicator}{Jobkeeper applications, Total businesses, Jobkeeper proportion}
 #' \item{value}{Value}
 #' }
-#' 
+#'
 #' @source \url{https://treasury.gov.au/coronavirus/jobkeeper/data}
-#' 
+#'
 "jobkeeper_sa2"
 
 #' JobSeeker data by SA2
-#' 
+#'
 #' Department of Social Services estimates of monthly jobseeker and
-#' youth allowance payments since March 2020. 
-#' 
+#' youth allowance payments since March 2020.
+#'
 #' @format A dataframe with 4 variables
 #' \describe{
 #' \item{sa2_main_2016}{Statistical Area 2 Code}
@@ -149,13 +149,13 @@
 #' \item{value}{Value}
 #' }
 #'
-#' @source \url{https://data.gov.au/data/dataset/jobseeker-payment-and-youth-allowance-recipients-monthly-profile} 
+#' @source \url{https://data.gov.au/data/dataset/jobseeker-payment-and-youth-allowance-recipients-monthly-profile}
 "jobseeker_sa2"
 
-#' Labour Account 
-#' 
-#' Experimental ABS labour force data 
-#' 
+#' Labour Account
+#'
+#' Experimental ABS labour force data
+#'
 #' @format A dataframe with 10 variables
 #' \describe{
 #' \item{date}{Date}
@@ -168,15 +168,15 @@
 #' \item{value}{Value}
 #' \item{unit}{Unit of value}
 #' }
-#' 
+#'
 #' @source \url{https://www.abs.gov.au/ausstats/abs@.nsf/mf/6150.0.55.003}
 "labour_account"
 
 #' ABS Labour Force Survey
-#' 
+#'
 #' A dataset containing monthly national and sub-national labour force
-#' indicators since February 1978. 
-#' 
+#' indicators since February 1978.
+#'
 #' @format A dataframe with 10 variables:
 #' \describe{
 #' \item{date}{date of survey}
@@ -190,14 +190,14 @@
 #' \item{indicator}{labour force indicator}
 #' \item{value}{value of labour force indicator}
 #' }
-#' 
+#'
 #' @source \url{https://www.abs.gov.au/ausstats/abs@.nsf/mf/6202.0}
 "labour_force"
 
 #' Payroll Index
-#' 
-#' ABS Weekly Payroll data since 4th January 2020. 
-#' 
+#'
+#' ABS Weekly Payroll data since 4th January 2020.
+#'
 #' @format A dataframe with 6 variables:
 #' \describe{
 #' \item{date}{Date}
@@ -207,14 +207,14 @@
 #' \item{industry}{Industry}
 #' \item{value}{Value}
 #' }
-#' 
+#'
 #' @source \url{https://www.abs.gov.au/ausstats/abs@.nsf/mf/6160.0.55.001}
 "payroll_index"
 
 #' Payroll SA4
-#' 
+#'
 #' ABS Weekly Payroll data for Statistical Area 4 Regions
-#' 
+#'
 #' @format A dataframe with 5 variables:
 #' \describe{
 #' \item{state_name_2016}{State}
@@ -222,16 +222,16 @@
 #' \item{value}{Number of payroll jobs - indexed to March 14th 2020}
 #' \item{indicator}{This dataset contains only the payroll index data}
 #' }
-#' 
+#'
 #' @source \url{https://www.abs.gov.au/ausstats/abs@.nsf/mf/6160.0.55.001}
 "payroll_sa4"
 
 #' Small Area Labour Market
-#' 
-#' The Department of Education, Skills and Employment quarterly regional 
+#'
+#' The Department of Education, Skills and Employment quarterly regional
 #' estimates of unemployment and the unemployment rate at the Statistical
 #' Area Level 2 (SA2)
-#' 
+#'
 #' @format A dataframe with 6 variables:
 #' \describe{
 #' \item{indicator}{One of Labour force total, Unemployment rate, Unemployed total}
@@ -241,7 +241,6 @@
 #' \item{value}{Value of the indicator}
 #' \item{state_name_2016}{State name}
 #' }
-#' 
+#'
 #' @source \url{https://www.employment.gov.au/small-area-labour-markets-publication-0}
 "small_area_labour_market"
-

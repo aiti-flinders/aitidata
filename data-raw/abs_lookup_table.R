0f61205e97ee4b17ad654d9613ca1eddc5320c0e
Hamish Gamble <hamish.gamble@gmail.com>
2020-11-25 11:47:01 +1030

    Updating now uses an internal function update_abs_lookup_table()

--- data-raw/abs_lookup_table.R
+++ data-raw/abs_lookup_table.R
@@ -3,6 +3,6 @@ library(daitir)
 library(dplyr)
 library(stringr)
 
-abs_lookup_table <- daitir::update_abs_lookup_table()
+abs_lookup_table <- daitir:::update_abs_lookup_table()
 
 usethis::use_data(abs_lookup_table, overwrite = TRUE, internal = TRUE)

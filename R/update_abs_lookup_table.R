77cf86cf7e184d487ebed03e5ec081d2bbb036c8
Hamish Gamble <hamish.gamble@gmail.com>
2020-11-25 14:26:26 +1030

    Make next_release a date if the scrape returns nothing so that it can be compared with todays date

--- R/update_abs_lookup_table.R
+++ R/update_abs_lookup_table.R
@@ -31,7 +31,7 @@ update_abs_lookup_table <- function() {
   scrape_next_release <- function(url) {
     next_release <- xml2::read_html(url) %>% rvest::html_nodes(xpath = '//*[@id="release-date-section"]/div[2]/div/div/ul/li[1]/span/text()') %>% rvest::html_text() 
     if (length(next_release) == 0) {
-      next_release <- "N/A" 
+      next_release <- as.Date("01-01-1900") 
     } else { next_release <- next_release %>% 
       stringi::stri_replace_all_fixed("Next Release ", "")
     }

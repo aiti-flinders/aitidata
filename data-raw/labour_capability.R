31b6f79398b544728e50b8e9b6d4da4365454cec
Hamish Gamble <hamish.gamble@gmail.com>
2021-05-28 14:55:47 +0930

    document

--- data-raw/labour_capability.R
+++ data-raw/labour_capability.R
@@ -9,10 +9,7 @@ sub_100_businesses <- input_data %>%
   filter(total < 1807) %>%
   pull(sa2_name_2016)
 
-input_data <- read_csv("data-raw/degree2_2016_ur.csv", skip = 9, n_max = 88) 
-
-
-%>%
+input_data <- read_csv("data-raw/degree2_2016_ur.csv", skip = 9, n_max = 88) %>%
   select(sa2_name_2016 = "SA2 (UR)",
          occupation = "OCCP - 2 Digit Level",
          total = "Count") %>%

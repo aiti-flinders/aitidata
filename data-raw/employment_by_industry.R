5d792219a7e75fe67096940f2a452938aad219b6
Hamish Gamble <hamish.gamble@gmail.com>
2021-05-28 14:49:38 +0930

    Only read table 5 (employment by industry)

--- data-raw/employment_by_industry.R
+++ data-raw/employment_by_industry.R
@@ -1,16 +1,14 @@
 ## code to prepare `employment_by_industry` dataset goes here.
 library(readabs)
-library(aitidata)
-library(lubridate)
 library(dplyr)
 library(tidyr)
-library(stringr)
-library(purrr)
-library(readxl)
+library(lubridate)
 
-abs_test <- download_data_cube("labour-force-australia-detailed", "6291023a.xls") 
 
-abs_file <- read_abs_local(filenames = "6291023a.xls", path = "data-raw")
+
+abs_test <- aitidata::download_data_cube("labour-force-australia-detailed", "6291023a.xls") 
+
+abs_file <- readabs::read_abs_local(filenames = "6291023a.xls", path = "data-raw")
 
 if (max(abs_file$date) <= max(aitidata::employment_by_industry$date)) {
   message("Skipping `employment_by_industry.rda`: appears to be up-to-date")
@@ -18,53 +16,25 @@ if (max(abs_file$date) <= max(aitidata::employment_by_industry$date)) {
 } else {
   message("updating `employment_by_industry`")
   
-  raw <- read_abs("6291.0.55.001", tables = c(5, 19), retain_files = FALSE)
+  raw <- aitidata::download_data_cube("labour-force-australia-detailed", "6291005.xls")
   
-  employment_industry_5 <- raw %>%
-    filter(table_no == "6291005") %>%
-    separate(series, into = c("state", "industry", "indicator"), sep = ";", extra = "drop") %>%
-    mutate(
-      across(c("state", "industry", "indicator"), ~ str_remove_all(., "> ")),
-      across(where(is.character), ~ trimws(.)),
-      indicator = ifelse(indicator == "", industry, indicator),
-      industry = ifelse(str_detect(industry, "Employed"), "Total (industry)", industry),
-      gender = "Persons",
-      age = "Total (age)",
-      year = year(date),
-      month = month(date, label = TRUE, abbr = FALSE),
-      value = ifelse(unit == "000", value * 1000, value)
-    ) %>%
-    select(date, year, month, indicator, industry, gender, age, state, series_type, value, unit)  %>% 
-    group_by(date,  indicator, gender, age, state) %>% 
-    mutate(value_share = 200 * value/sum(value)) %>%
-    ungroup()
+  employment_by_industry <- readabs::read_abs_local(filenames = raw, path = "data-raw") %>%
+    tidyr::separate(series, into = c("state", "industry", "indicator"), sep = ";", extra = "drop") %>%
+    dplyr::mutate(dplyr::across(c("state", "industry", "indicator"), ~ gsub(pattern = "> ", x = .x, replacement = "")),
+                  dplyr::across(where(is.character), ~trimws(.x)),
+                  indicator = ifelse(indicator == "", industry, indicator),
+                  industry = ifelse(grepl(x = industry, pattern = "Employed"), "Total (industry)", industry),
+                  gender = "Persons",
+                  age = "Total (age)",
+                  year = lubridate::year(date),
+                  month = lubridate::month(date, label = TRUE, abbr = FALSE),
+                  value = ifelse(unit == "000", value * 1000, value)) %>%
+    dplyr::group_by(date,  indicator, gender, age, state) %>% 
+    dplyr::mutate(value_share = 200 * value/sum(value)) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(date, year, month, indicator, industry, gender, age, state, series_type, value, unit) 
   
-  
-  employment_industry_19 <- raw %>%
-    filter(table_no == "6291019") %>%
-    separate(series, into = c("industry", "indicator", "gender"), sep = ";", extra = "drop") %>%
-    mutate(across(c("industry", "indicator", "gender"), ~ str_remove_all(., "> ")),
-           across(where(is.character), ~ trimws(.)),
-           year = year(date),
-           month = month(date, label = TRUE, abbr = FALSE),
-           value = ifelse(unit == "000", value * 1000, value),
-           state = "Australia",
-           age = "Total (age)",
-           gender = ifelse(gender == "", indicator, gender),
-           indicator = ifelse(indicator %in% gender, industry, indicator),
-           industry = ifelse(industry %in% indicator, "Total (industry)", industry)
-    ) %>%
-    filter(!industry %in% c("Managers", "Professionals", "Technicians and Trades Workers", "Community and Personal Service Workers", "Clerical and Administrative Workers", "Sales Workers", "Machinery Operators and Drivers", "Labourers")) %>%
-    select(date, year, month, indicator, industry, gender, age, state, series_type, value, unit) %>% 
-    group_by(date,  indicator, gender, age, state) %>% 
-    mutate(value_share = 200 * value/sum(value)) %>%
-    ungroup()
-  
-  
-  employment_by_industry <- bind_rows(employment_industry_5, employment_industry_19) %>%
-    distinct()
-  
-  
-  save(employment_by_industry, file = here::here("data", "employment_by_industry.rda"), compress = "xz")
+  usethis::use_data(employment_by_industry, overwrite = TRUE, compress = "xz")
+  file.remove(raw)
   
 }

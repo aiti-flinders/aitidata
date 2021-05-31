74c2b418643ba9d19a26c9bb6895f0ed7f6bb7da
Hamish <hamish.gamble@flinders.edu.au>
2021-05-31 13:34:10 +0930

    tidy (again!)

--- data-raw/aus_manufacturing.R
+++ data-raw/aus_manufacturing.R
@@ -1,12 +1,18 @@
 ## code to prepare `aus_manufacturing` dataset goes here
+library(dplyr)
+library(purrr)
+library(stringr)
+library(tidyr)
+library(readxl)
 
 
-abs_file <- purrr::map_chr(.x = "Manufacturing industry", .f = ~aitidata::download_data_cube(catalogue_string = "australian-industry",
+abs_file <- map_chr(.x = c("Manufacturing industry"),
+                    .f = ~download_data_cube(catalogue_string = "australian-industry",
                                              cube = .x, 
                                              path = "data-raw"))
 #fpath <- gsub("[^\\/]*$", "", fpath)
-years <- readxl::read_xls(abs_file, sheet = "Table_1", skip = 4, n_max = 0) %>%
-  dplyr::select(1, 5, 9) %>%
+years <- read_xls(abs_file, sheet = "Table_1", skip = 4, n_max = 0) %>%
+  select(1, 5, 9) %>%
   colnames()
 
 if (max(years) == max(aus_manufacturing$year)) {
@@ -14,27 +20,27 @@ if (max(years) == max(aus_manufacturing$year)) {
   file.remove(abs_file)
 } else {
   
-  years <- readxl::read_xls(abs_file, sheet = "Table_1", skip = 4, n_max = 0) %>%
-    dplyr::select(1, 5, 9) %>%
+  years <- read_xls(abs_file, sheet = "Table_1", skip = 4, n_max = 0) %>%
+    select(1, 5, 9) %>%
     colnames()
   
-  aus_manufacturing <- readxl::read_xls(abs_file, 
+  aus_manufacturing <- read_xls(abs_file, 
                                 sheet = "Table_1", 
                                 skip = 7, 
                                 col_names = c("industry", "employment_1", "wages_1", "income_1", "iva_1",
                                               "employment_2", "wages_2", "income_2", "iva_2",
                                               "employment_3", "wages_3", "income_3", "iva_3")) %>%
-    tidyr::pivot_longer(cols = 2:length(.), names_to = "indicator", values_to = "value") %>%
-    dplyr::mutate(year = dplyr::case_when(
-      stringr::str_detect(indicator, "_1") ~ years[1],
-      stringr::str_detect(indicator, "_2") ~ years[2],
-      stringr::str_detect(indicator, "_3") ~ years[3]),
-      industry_code = stringr::str_extract(industry, "\\d{2,4}"),
-      industry = trimws(stringr::str_replace(industry, "\\d{2,4}", "")),
-      indicator = stringr::str_remove_all(indicator, "_[1-3]"))
+    pivot_longer(cols = 2:length(.), names_to = "indicator", values_to = "value") %>%
+    mutate(year = case_when(
+      str_detect(indicator, "_1") ~ years[1],
+      str_detect(indicator, "_2") ~ years[2],
+      str_detect(indicator, "_3") ~ years[3]
+    )) %>%
+    mutate(industry_code = str_extract(industry, "\\d{2,4}"),
+           industry = trimws(str_replace(industry, "\\d{2,4}", ""))) %>%
+    mutate(indicator = str_remove_all(indicator, "_[1-3]"))
   
   file.remove(abs_file)
 }
 
 usethis::use_data(aus_manufacturing, overwrite = TRUE)
-

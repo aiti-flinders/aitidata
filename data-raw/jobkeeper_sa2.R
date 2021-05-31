a47febfdc5ddaac16949cfd2d10b0a3bf084460e
Hamish <hamish.gamble@flinders.edu.au>
2021-05-31 14:09:35 +0930

    Always check for jobkeeper updates

--- data-raw/jobkeeper_sa2.R
+++ data-raw/jobkeeper_sa2.R
@@ -72,26 +72,6 @@ if (!file.exists("data-raw/abs_poa_2016.zip")) {
   )
 }
 
-jobkeeper_url <- "https://treasury.gov.au/coronavirus/jobkeeper/data"
-
-release_page <- rvest::read_html(jobkeeper_url)
-
-jobkeeper_data_url <- release_page %>%
-  rvest::html_nodes(xpath = '//*[@id="block-mainpagecontent-2"]/div/article/div/div/table/tbody/tr/td[2]/p') %>%
-  rvest::html_children() %>%
-  rvest::html_attr("href") %>%
-  .[1]
-
-jobkeeper_date <- release_page %>%
-  rvest::html_text() %>%
-  stringr::str_extract("(?<=\\: ).*") %>%
-  as.Date(format = "%d %B %Y")
-
-#Many conditions for updating this dataset:
-#1. New meshblock files
-#2. New jobkeeper data
-#3. New business count data
-
 get_file_types <- function(file, sheet, skip, n_max) {
   nms <- names(readxl::read_excel(path = file, sheet = sheet, skip = skip, n_max = 0))
   nms <- tolower(nms)
@@ -119,115 +99,111 @@ read_jobkeeper <- function(file, sheet, ...) {
   return(jobkeeper_postal)
 }
 
+message("Updating jobkeeper_sa2 dataset...")
 
-if (!as.Date(file.info("data/jobkeeper_sa2.rda")$mtime) >= jobkeeper_date | !file.exists("data/jobkeeper_sa2.rda")) {
-  message("Updating jobkeeper_sa2 dataset...")
+download.file(paste0("https://treasury.gov.au/", jobkeeper_data_url), dest = "data-raw/jobkeeper_postal.xlsx", mode = "wb")
 
-  download.file(paste0("https://treasury.gov.au/", jobkeeper_data_url), dest = "data-raw/jobkeeper_postal.xlsx", mode = "wb")
-  
-  jobkeeper_phase_1 <- read_jobkeeper("data-raw/jobkeeper_postal.xlsx", 
-                                      sheet = 2)
-  
-  jobkeeper_phase_2 <- read_jobkeeper("data-raw/jobkeeper_postal.xlsx",
-                                      sheet = 3)
-  
-  jobkeeper_phase_3 <- read_jobkeeper("data-raw/jobkeeper_postal.xlsx",
-                                      sheet = 4,
-                                      range = readxl::cell_limits(c(3, 1), c(NA, 3)))
-  
-  jobkeeper_all <- dplyr::left_join(jobkeeper_phase_1, jobkeeper_phase_2, by = "postcode") %>%
-    dplyr::left_join(jobkeeper_phase_3, by = "postcode")
+jobkeeper_phase_1 <- read_jobkeeper("data-raw/jobkeeper_postal.xlsx", 
+                                    sheet = 2)
+
+jobkeeper_phase_2 <- read_jobkeeper("data-raw/jobkeeper_postal.xlsx",
+                                    sheet = 3)
+
+jobkeeper_phase_3 <- read_jobkeeper("data-raw/jobkeeper_postal.xlsx",
+                                    sheet = 4,
+                                    range = readxl::cell_limits(c(3, 1), c(NA, 3)))
+
+jobkeeper_all <- dplyr::left_join(jobkeeper_phase_1, jobkeeper_phase_2, by = "postcode") %>%
+  dplyr::left_join(jobkeeper_phase_3, by = "postcode")
 
 
 
-  business_sa2 <- aitidata::cabee_sa2 %>%
-    dplyr::filter(indicator == "total") %>%
-    dplyr::group_by(date, sa2_main_2016, sa2_name_2016) %>%
-    dplyr::summarise(total_businesses = sum(value, na.rm = T), .groups = "drop") %>%
-    dplyr::ungroup() %>%
-    dplyr::filter(date == max(.$date)) %>%
-    dplyr::select(-date, -sa2_name_2016)
+business_sa2 <- aitidata::cabee_sa2 %>%
+  dplyr::filter(indicator == "total") %>%
+  dplyr::group_by(date, sa2_main_2016, sa2_name_2016) %>%
+  dplyr::summarise(total_businesses = sum(value, na.rm = T), .groups = "drop") %>%
+  dplyr::ungroup() %>%
+  dplyr::filter(date == max(.$date)) %>%
+  dplyr::select(-date, -sa2_name_2016)
 
-  jobkeeper_sa2 <- dplyr::left_join(postal_areas, mesh_aus, by = "MB_CODE_2016") %>%
-    dplyr::left_join(jobkeeper_all, by = c("POA_CODE_2016" = "postcode")) %>%
-    dplyr::left_join(business_sa2, by = c("SA2_MAINCODE_2016" = "sa2_main_2016")) %>%
-    dplyr::group_by(POA_CODE_2016) %>%
-    dplyr::mutate(share = total_businesses / sum(total_businesses, na.rm = T)) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(dplyr::across(tidyr::contains("apps_"), .fns = ~. * share, .names = "{.col}")) %>%
-    dplyr::group_by(SA2_MAINCODE_2016) %>%
-    dplyr::summarise(dplyr::across(tidyr::contains("apps_"), .fns = ~sum(., na.rm = TRUE), .names = "{.col}")) %>%
-    tidyr::pivot_longer(cols = 2:length(.),
-                        names_to = "date",
-                        values_to = "jobkeeper_applications") %>%
-    dplyr::mutate(date = stringr::str_remove(date, "apps_"),
-                  month = match(date, tolower(month.name)),
-                  year = dplyr::case_when(month %in% c(1,2,3) ~ 2021,
-                                   TRUE ~ 2020),
-                  date = as.Date(paste(year, month,  "01", sep = "-")),
-                  jobkeeper_applications = ceiling(jobkeeper_applications)) %>%
-    dplyr::select(-year, -month) %>%
-    dplyr::left_join(business_sa2, by = c("SA2_MAINCODE_2016" = "sa2_main_2016")) %>%
-    dplyr::mutate(jobkeeper_proportion = ifelse(total_businesses != 0, 100 * jobkeeper_applications / total_businesses, 0)) %>%
-    dplyr::filter(!is.na(SA2_MAINCODE_2016),
-                  !SA2_MAINCODE_2016 %in% c("197979799",
-                                        "297979799",
-                                        "397979799",
-                                        "497979799",
-                                        "597979799",
-                                        "697979799",
-                                        "797979799",
-                                        "897979799")) %>%
-    tidyr::pivot_longer(cols = c(-SA2_MAINCODE_2016, -date),
-                        names_to = "indicator",
-                        values_to = "value") %>%
-    dplyr::rename(sa2_main_2016 = SA2_MAINCODE_2016) %>%
-    dplyr::mutate(dplyr::across(indicator, ~ stringr::str_to_sentence(stringr::str_replace_all(., "_", " "))),
-                  unit = dplyr::case_when(indicator == "Jobkeeper proportion" ~ "Percent", TRUE ~ "000"),
-                  series_type = "Original",
-                  month = lubridate::month(date, abbr = FALSE, label = TRUE),
-                  year = lubridate::year(date),
-                  gender = "Persons",
-                  age = "Total (age)")
-  
-  jobkeeper_state <- jobkeeper_sa2 %>% 
-    tidyr::pivot_wider(id_cols = c(sa2_main_2016, date), 
-                       names_from = indicator, 
-                       values_from = value) %>% 
-    dplyr::left_join(mesh_aus[c("SA2_MAINCODE_2016", "STATE_NAME_2016")], by = c("sa2_main_2016" = "SA2_MAINCODE_2016")) %>% 
-    dplyr::group_by(STATE_NAME_2016, date) %>% 
-    dplyr::summarise(dplyr::across(c(`Jobkeeper applications`, `Total businesses`), sum), .groups = "drop") %>%
-    dplyr::ungroup() %>%
-    dplyr::filter(STATE_NAME_2016 != "Other Territories") %>%
-    tidyr::pivot_longer(cols = c(`Jobkeeper applications`, `Total businesses`), 
-                        names_to = "indicator", 
-                        values_to = "value") %>%
-    tidyr::pivot_wider(names_from = STATE_NAME_2016, 
-                       values_from = value) %>%
-    dplyr::rowwise() %>% 
-    dplyr::mutate(Australia = rowSums(dplyr::across(c(3:10)))) %>%
-    dplyr::ungroup() %>% 
-    tidyr::pivot_longer(cols = 3:11,
-                        names_to = "state", 
-                        values_to = "value") %>%
-    tidyr::pivot_wider(names_from = indicator, 
-                       values_from = value) %>%
-    dplyr::mutate("Jobkeeper proportion" = 100*`Jobkeeper applications`/`Total businesses`) %>%
-    tidyr::pivot_longer(cols = 3:5, 
-                        names_to = "indicator", 
-                        values_to = "value") %>%
-    dplyr::mutate(unit = dplyr::case_when(indicator == "Jobkeeper proportion" ~ "Percent", TRUE ~ "000"),
-                  series_type = "Original",
-                  month = lubridate::month(date, abbr = FALSE, label = TRUE),
-                  year = lubridate::year(date),
-                  gender = "Persons",
-                  age = "Total (age)")
+jobkeeper_sa2 <- dplyr::left_join(postal_areas, mesh_aus, by = "MB_CODE_2016") %>%
+  dplyr::left_join(jobkeeper_all, by = c("POA_CODE_2016" = "postcode")) %>%
+  dplyr::left_join(business_sa2, by = c("SA2_MAINCODE_2016" = "sa2_main_2016")) %>%
+  dplyr::group_by(POA_CODE_2016) %>%
+  dplyr::mutate(share = total_businesses / sum(total_businesses, na.rm = T)) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(dplyr::across(tidyr::contains("apps_"), .fns = ~. * share, .names = "{.col}")) %>%
+  dplyr::group_by(SA2_MAINCODE_2016) %>%
+  dplyr::summarise(dplyr::across(tidyr::contains("apps_"), .fns = ~sum(., na.rm = TRUE), .names = "{.col}")) %>%
+  tidyr::pivot_longer(cols = 2:length(.),
+                      names_to = "date",
+                      values_to = "jobkeeper_applications") %>%
+  dplyr::mutate(date = stringr::str_remove(date, "apps_"),
+                month = match(date, tolower(month.name)),
+                year = dplyr::case_when(month %in% c(1,2,3) ~ 2021,
+                                        TRUE ~ 2020),
+                date = as.Date(paste(year, month,  "01", sep = "-")),
+                jobkeeper_applications = ceiling(jobkeeper_applications)) %>%
+  dplyr::select(-year, -month) %>%
+  dplyr::left_join(business_sa2, by = c("SA2_MAINCODE_2016" = "sa2_main_2016")) %>%
+  dplyr::mutate(jobkeeper_proportion = ifelse(total_businesses != 0, 100 * jobkeeper_applications / total_businesses, 0)) %>%
+  dplyr::filter(!is.na(SA2_MAINCODE_2016),
+                !SA2_MAINCODE_2016 %in% c("197979799",
+                                          "297979799",
+                                          "397979799",
+                                          "497979799",
+                                          "597979799",
+                                          "697979799",
+                                          "797979799",
+                                          "897979799")) %>%
+  tidyr::pivot_longer(cols = c(-SA2_MAINCODE_2016, -date),
+                      names_to = "indicator",
+                      values_to = "value") %>%
+  dplyr::rename(sa2_main_2016 = SA2_MAINCODE_2016) %>%
+  dplyr::mutate(dplyr::across(indicator, ~ stringr::str_to_sentence(stringr::str_replace_all(., "_", " "))),
+                unit = dplyr::case_when(indicator == "Jobkeeper proportion" ~ "Percent", TRUE ~ "000"),
+                series_type = "Original",
+                month = lubridate::month(date, abbr = FALSE, label = TRUE),
+                year = lubridate::year(date),
+                gender = "Persons",
+                age = "Total (age)")
 
-  usethis::use_data(jobkeeper_state, overwrite = TRUE, compress = "xz")
-  usethis::use_data(jobkeeper_sa2, overwrite = TRUE, compress = "xz")
+jobkeeper_state <- jobkeeper_sa2 %>% 
+  tidyr::pivot_wider(id_cols = c(sa2_main_2016, date), 
+                     names_from = indicator, 
+                     values_from = value) %>% 
+  dplyr::left_join(mesh_aus[c("SA2_MAINCODE_2016", "STATE_NAME_2016")], by = c("sa2_main_2016" = "SA2_MAINCODE_2016")) %>% 
+  dplyr::group_by(STATE_NAME_2016, date) %>% 
+  dplyr::summarise(dplyr::across(c(`Jobkeeper applications`, `Total businesses`), sum), .groups = "drop") %>%
+  dplyr::ungroup() %>%
+  dplyr::filter(STATE_NAME_2016 != "Other Territories") %>%
+  tidyr::pivot_longer(cols = c(`Jobkeeper applications`, `Total businesses`), 
+                      names_to = "indicator", 
+                      values_to = "value") %>%
+  tidyr::pivot_wider(names_from = STATE_NAME_2016, 
+                     values_from = value) %>%
+  dplyr::rowwise() %>% 
+  dplyr::mutate(Australia = rowSums(dplyr::across(c(3:10)))) %>%
+  dplyr::ungroup() %>% 
+  tidyr::pivot_longer(cols = 3:11,
+                      names_to = "state", 
+                      values_to = "value") %>%
+  tidyr::pivot_wider(names_from = indicator, 
+                     values_from = value) %>%
+  dplyr::mutate("Jobkeeper proportion" = 100*`Jobkeeper applications`/`Total businesses`) %>%
+  tidyr::pivot_longer(cols = 3:5, 
+                      names_to = "indicator", 
+                      values_to = "value") %>%
+  dplyr::mutate(unit = dplyr::case_when(indicator == "Jobkeeper proportion" ~ "Percent", TRUE ~ "000"),
+                series_type = "Original",
+                month = lubridate::month(date, abbr = FALSE, label = TRUE),
+                year = lubridate::year(date),
+                gender = "Persons",
+                age = "Total (age)")
+
+usethis::use_data(jobkeeper_state, overwrite = TRUE, compress = "xz")
+usethis::use_data(jobkeeper_sa2, overwrite = TRUE, compress = "xz")
+
+file.remove("data-raw/jobkeeper_postal.xlsx")
   
-  file.remove("data-raw/jobkeeper_postal.xlsx")
-  
-} else {
-  message("jobkeeper_sa2 data is already up to date")
-}
+

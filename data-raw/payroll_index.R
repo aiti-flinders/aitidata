94141742574e9480d5bc6bf84ab6784ad0a46e9b
Hamish <hamish.gamble@flinders.edu.au>
2021-08-19 16:31:26 +0930

    remove industry wages and clean state abbreviations to state names

--- data-raw/payroll_index.R
+++ data-raw/payroll_index.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(tidyr)
 library(readxl)
 library(readabs)
+library(strayr)
 
 
 abs_test <- readabs::read_payrolls("industry_jobs", path = "data-raw")
@@ -16,15 +17,17 @@ if (max(abs_test$date) <= max(aitidata::payroll_index$date)) {
   payroll_jobs <- readabs::read_payrolls("industry_jobs", path = "data-raw") %>%
     dplyr::rename(gender = sex,
                   indicator = series) %>%
-    dplyr::mutate(indicator = "payroll_jobs")
+    dplyr::mutate(indicator = "payroll_jobs",
+                  state = strayr::clean_state(state, to = "state_name"))
   
-  payroll_wages <- readabs::read_payrolls("industry_wages", path = "data-raw") %>%
-    dplyr::rename(gender = sex,
-                  indicator = series) %>%
-    dplyr::mutate(indicator = "payroll_wages")
+  # payroll_wages <- readabs::read_payrolls("industry_wages", path = "data-raw") %>%
+  #   dplyr::rename(gender = sex,
+  #                 indicator = series) %>%
+  #   dplyr::mutate(indicator = "payroll_wages",
+  #                 state = strayr::clean_state(state, to = "state_name"))
+  # 
   
-  
-  payroll_index <- dplyr::bind_rows(payroll_jobs, payroll_wages)
+  # payroll_index <- dplyr::bind_rows(payroll_jobs, payroll_wages)
   
   file.remove("data-raw/6160055001_DO004.xlsx")
   

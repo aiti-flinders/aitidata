7bcb9833cb1de5c4f4ce38b671e77ea2b6efa5a4
Hamish <hamish.gamble@flinders.edu.au>
2021-09-17 15:41:56 +0930

    Manual update to address name change in absmapsdata

--- data-raw/small_area_labour_market.R
+++ data-raw/small_area_labour_market.R
@@ -25,7 +25,7 @@ if (current_date <= max(aitidata::small_area_labour_market$date)) {
   raw <- readr::read_csv("data-raw/salm_sa2.csv", skip = 1)
   
   all_sa2 <- data.frame(absmapsdata::sa22016) %>%
-    dplyr::select(sa2_name_2016, sa2_main_2016, state_name_2016)
+    dplyr::select(sa2_name_2016, sa2_code_2016, state_name_2016)
   
   small_area_labour_market <- raw %>%
     dplyr::mutate(dplyr::across(where(is.numeric), as.character)) %>%
@@ -34,11 +34,11 @@ if (current_date <= max(aitidata::small_area_labour_market$date)) {
                         values_to = "value") %>%
     dplyr::rename(indicator = `Data Item`,
                   sa2_name_2016 = `Statistical Area Level 2 (SA2) (2016 ASGS)`,
-                  sa2_main_2016 = `SA2 Code (2016 ASGS)`) %>%
+                  sa2_code_2016 = `SA2 Code (2016 ASGS)`) %>%
     dplyr::mutate(value = as.numeric(gsub(",", "", value)),
                   date = as.Date(paste0(date, "-01"), format = "%b-%y-%d")) %>%
     dplyr::right_join(all_sa2) %>%
-    tidyr::complete(indicator, tidyr::nesting(sa2_name_2016, sa2_main_2016), date) %>%
+    tidyr::complete(indicator, tidyr::nesting(sa2_name_2016, sa2_code_2016), date) %>%
     dplyr::filter(!is.na(date), !is.na(indicator))
 
 usethis::use_data(small_area_labour_market, overwrite = TRUE, compress = "xz")

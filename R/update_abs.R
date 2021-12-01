2a3c6863af08ac4ed001bddb59db860b7f73dc45
Hamish <hamish.gamble@flinders.edu.au>
2021-12-01 11:12:55 +1030

    Explicitly reference aitidata_catalogues

--- R/update_abs.R
+++ R/update_abs.R
@@ -4,6 +4,6 @@
 #' 
 #'
 update_abs <- function() {
-    file_paths <- purrr::map_chr(unique(aitidata_catalogues$data_name), ~here::here("data-raw", paste0(., ".R")))
+    file_paths <- purrr::map_chr(unique(aitidata::aitidata_catalogues$data_name), ~here::here("data-raw", paste0(., ".R")))
     purrr::map(file_paths, source)
 }

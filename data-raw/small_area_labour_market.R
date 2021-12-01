0806e1019e9726b47aa017f4f2ebb2a1338ef885
Hamish <hamish.gamble@flinders.edu.au>
2021-12-01 14:55:19 +1030

    Uses read_absmap instead of absmapsdata

--- data-raw/small_area_labour_market.R
+++ data-raw/small_area_labour_market.R
@@ -24,7 +24,7 @@ if (current_date <= max(aitidata::small_area_labour_market$date)) {
   
   raw <- readr::read_csv("data-raw/salm_sa2.csv", skip = 1)
   
-  all_sa2 <- data.frame(absmapsdata::sa22016) %>%
+  all_sa2 <- data.frame(read_absmap("sa22016")) %>%
     dplyr::select(sa2_name_2016, sa2_code_2016, state_name_2016)
   
   small_area_labour_market <- raw %>%

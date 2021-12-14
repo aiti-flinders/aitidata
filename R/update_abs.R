fbf13f46e5f0e1e24df213797e7060c40557334f
Hamish <hamish.gamble@flinders.edu.au>
2021-12-14 11:52:12 +1030

    Exports update_abs()

--- R/update_abs.R
+++ R/update_abs.R
@@ -2,7 +2,7 @@
 #'
 #' @return NULL
 #' 
-#'
+#' @export
 update_abs <- function() {
     file_paths <- purrr::map_chr(unique(aitidata::aitidata_catalogues$data_name), ~here::here("data-raw", paste0(., ".R")))
     purrr::map(file_paths, source)

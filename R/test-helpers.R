fc356a413272aeaa3e7c95db2bcda5e21d5b60c5
Hamish <hamish.gamble@flinders.edu.au>
2022-02-11 09:37:04 +1030

    Adds functions to help tests

---
+++ R/test-helpers.R
@@ -0,0 +1,17 @@
+local_create_package <- function(dir = file_temp(), env = parent.frame()) {
+  old_project <- usethis::proj_get()
+  
+  # create new folder and package
+  usethis::create_package(dir, open = FALSE) # A
+  withr::defer(fs::dir_delete(dir), envir = env) # -A
+  
+  # change working directory
+  setwd(dir) # B
+  withr::defer(setwd(old_project), envir = env) # -B
+  
+  # switch to new usethis project
+  usethis::proj_set(dir) # C
+  withr::defer(proj_set(old_project, force = TRUE), envir = env) # -C
+  
+  dir
+}

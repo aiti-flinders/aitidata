d12b68dc09a3aabe2a20d83321ffecb6ebcc2294
Hamish <hamish.gamble@flinders.edu.au>
2022-02-18 15:08:13 +1030

    Updates more data

--- R/update_abs.R
+++ R/update_abs.R
@@ -5,19 +5,22 @@
 #' @return NULL
 #' 
 update_abs <- function(force_update = FALSE) {
-  update_labour_force(force_update)
-  update_employment_by_industry(force_update)
-  update_cabee_sa2(force_update)
   update_internet_vacancies_regional(force_update)
+  update_internet_vacancies_index(force_update)
+  update_small_area_labour_market(force_update)
+  update_industry_employment(force_update)
+  update_industry_underemployment(force_update)
+  update_industry_employment_detailed(force_update)
   update_aus_manufacturing(force_update)
-  update_labour_account(force_update) 
   update_mobility_facebook()
-  update_national_accounts(force_update) 
+  update_national_accounts(force_update)
+  update_underutilisation(force_update)
+  update_labour_account(force_update)
+  update_jobseeker_sa2(force_update)
+  update_labour_force(force_update)
+  update_retail_trade(force_update)
+  update_cabee_sa2(force_update)
   update_payroll_index(force_update)
   update_payroll_substate(force_update)
-  update_retail_trade(force_update)
-  update_underutilisation(force_update) 
-  update_small_area_labour_market(force_update)
-  update_jobseeker_sa2(force_update)
   update_covid_data() #this has to be after jobseeker, payroll, small area
 }

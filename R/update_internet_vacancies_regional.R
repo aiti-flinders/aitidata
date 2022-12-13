f1a2e0e9b0bef7c562578181f3b8c93ff1f2676d
Hamish Gamble <gamb0043@flinders.edu.au>
2022-12-13 16:47:31 +1030

    Check what GET is returning during GA

--- R/update_internet_vacancies_regional.R
+++ R/update_internet_vacancies_regional.R
@@ -11,7 +11,7 @@ update_internet_vacancies_regional <- function(force_update = FALSE) {
   if (!force_update) {
   
     dl <- httr::GET(
-      url = "https://www.nationalskillscommission.gov.au/sites/default/files/2022-10/IVI_DATA_SkillLevel%20-%20January%202006%20onwards.xlsx",
+      url = "https://www.jobsandskills.gov.au/sites/default/files/2022-11/IVI_DATA_SkillLevel%20-%20January%202006%20onwards.xlsx",
       header = httr::add_headers(header)
       )
   
@@ -36,47 +36,49 @@ update_internet_vacancies_regional <- function(force_update = FALSE) {
     message("Updating `internet_vacancies_regional` dataset.")
     
     dl <- httr::GET(
-      url = "https://www.nationalskillscommission.gov.au/sites/default/files/2022-10/IVI_DATA_regional%20-%20May%202010%20onwards.xlsx",
+      url = "https://www.jobsandskills.gov.au/sites/default/files/2022-11/IVI_DATA_regional%20-%20May%202010%20onwards.xlsx",
       header = httr::add_headers(header),
       httr::write_disk(here::here("data-raw/ivi_regional.xlsx"), overwrite = TRUE)
       )
+    
+    dl
 
           
-    internet_vacancies_regional <- readxl::read_excel(here::here("data-raw/ivi_regional.xlsx"),
-                                                      sheet = 2) %>%
-      tidyr::pivot_longer(cols = -c(.data$Level,
-                                    .data$State,
-                                    .data$region,
-                                    .data$ANZSCO_CODE,
-                                    .data$ANZSCO_TITLE),
-                          names_to = "date",
-                          values_to = "value") %>%
-      dplyr::mutate(date = as.Date(x = as.numeric(.data$date), origin = "1899-12-30"),
-                    unit = "000",
-                    state = strayr::clean_state(.data$State, to = "state_name"),
-                    ANZSCO_TITLE = ifelse(grepl("TOTAL", .data$ANZSCO_TITLE), "TOTAL", .data$ANZSCO_TITLE),
-                    region = dplyr::case_when(
-                      region == "Blue Mountains, Bathurst & Central West NSW" ~ "Blue Mountains Bathurst & Central West",
-                      region == "Central Queensland" ~ "Central QLD",
-                      region == "Far North Queensland" ~ "Far North QLD",
-                      region == 'Hobart & Southeast Tasmania' ~ "Hobart & Southeast TAS",
-                      region == "Launceston and Northeast Tasmania" ~ "Launceston and Northeast TAS", 
-                      region == "North West Tasmania" ~ "North West TAS",
-                      region == "Outback Queensland" ~ "Outback QLD",
-                      region == "Regional Northern Territory" ~ "Regional NT",
-                      TRUE ~ .data$region
-                    )) %>%
-      dplyr::select(.data$date,
-                    occupation_level = .data$Level,
-                    state = .data$State,
-                    vacancy_region = .data$region, 
-                    anzsco_code = .data$ANZSCO_CODE,
-                    anzsco_title = .data$ANZSCO_TITLE,
-                    .data$value)
-    
-    
-    usethis::use_data(internet_vacancies_regional, overwrite = TRUE, compress = 'xz')
-    file.remove(here::here("data-raw/ivi_regional.xlsx"))
+    # internet_vacancies_regional <- readxl::read_excel(here::here("data-raw/ivi_regional.xlsx"),
+    #                                                   sheet = 2) %>%
+    #   tidyr::pivot_longer(cols = -c(.data$Level,
+    #                                 .data$State,
+    #                                 .data$region,
+    #                                 .data$ANZSCO_CODE,
+    #                                 .data$ANZSCO_TITLE),
+    #                       names_to = "date",
+    #                       values_to = "value") %>%
+    #   dplyr::mutate(date = as.Date(x = as.numeric(.data$date), origin = "1899-12-30"),
+    #                 unit = "000",
+    #                 state = strayr::clean_state(.data$State, to = "state_name"),
+    #                 ANZSCO_TITLE = ifelse(grepl("TOTAL", .data$ANZSCO_TITLE), "TOTAL", .data$ANZSCO_TITLE),
+    #                 region = dplyr::case_when(
+    #                   region == "Blue Mountains, Bathurst & Central West NSW" ~ "Blue Mountains Bathurst & Central West",
+    #                   region == "Central Queensland" ~ "Central QLD",
+    #                   region == "Far North Queensland" ~ "Far North QLD",
+    #                   region == 'Hobart & Southeast Tasmania' ~ "Hobart & Southeast TAS",
+    #                   region == "Launceston and Northeast Tasmania" ~ "Launceston and Northeast TAS", 
+    #                   region == "North West Tasmania" ~ "North West TAS",
+    #                   region == "Outback Queensland" ~ "Outback QLD",
+    #                   region == "Regional Northern Territory" ~ "Regional NT",
+    #                   TRUE ~ .data$region
+    #                 )) %>%
+    #   dplyr::select(.data$date,
+    #                 occupation_level = .data$Level,
+    #                 state = .data$State,
+    #                 vacancy_region = .data$region, 
+    #                 anzsco_code = .data$ANZSCO_CODE,
+    #                 anzsco_title = .data$ANZSCO_TITLE,
+    #                 .data$value)
+    # 
+    # 
+    # usethis::use_data(internet_vacancies_regional, overwrite = TRUE, compress = 'xz')
+    # file.remove(here::here("data-raw/ivi_regional.xlsx"))
     
   } else {
     message("Skipping `internet_vacancies_regional.rda`: appears to be up-to-date")

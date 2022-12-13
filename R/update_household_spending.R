aca6ea94443da35f25b6ea35dbc784a353354c5f
Hamish Gamble <gamb0043@flinders.edu.au>
2022-12-13 13:26:00 +1030

    Household spending data can be read by readabs

--- R/update_household_spending.R
+++ R/update_household_spending.R
@@ -5,87 +5,49 @@
 
 update_household_spending <- function(force_update = FALSE) {
   
-  hh_spending <- function(path, state) {
-    
-    cur_month_header <- readxl::read_excel(path,
-                                           sheet = "Data 2",
-                                           range = "A4") %>%
-      colnames()
-    
-    cur_month <- paste0(stringr::str_extract(cur_month_header, "([^,]+$)"), " 01")
-    
-    rows <- lubridate::interval("2019-01-01", lubridate::myd(cur_month)) %/% months(1) + 1
-    
-    
-    readxl::read_excel(path, 
-                       sheet = "Data 2",
-                       skip = 4,
-                       n_max = rows,
-                       col_types = c("date",
-                                     rep("numeric", 14))) %>%
-      tidyr::pivot_longer(cols = 2:length(.),
-                          names_to = "coicop_division",
-                          values_to = "index") %>%
-      dplyr::mutate(date = as.Date(...1, origin = "1899-12-30"), 
-                    state = state) %>%
-      dplyr::filter(!is.na(index)) %>% 
-      dplyr::select(-...1)
-    
-  }
+  abs_test <- readabs::read_abs(cat_no = "5682.0", tables = "1", retain_files = FALSE)
   
-  if (!force_update) {
+  if (max(abs_test$date) > max(aitidata::household_spending$date) | force_update) {
     
-    fname <- aitidata::download_data_cube(catalogue_string = "monthly-household-spending-indicator",
-                                 cube = "1_National",
-                                 path = here::here("data-raw"))
+    message("Updating Monthly Household Spending Indicator")
     
-    # hh_spending <- function(path, state) {
-    #   
-    #   readxl::read_excel(path, 
-    #                      sheet = "Data 2",
-    #                      skip = 4,
-    #                      col_types = c("date",
-    #                                    rep("numeric", 14))) %>%
-    #     tidyr::pivot_longer(cols = 2:length(.),
-    #                         names_to = "coicop_division",
-    #                         values_to = "index") %>%
-    #     dplyr::mutate(date = as.Date(...1, origin = "1899-12-30"), 
-    #                   state = state) %>%
-    #     dplyr::filter(!is.na(index)) %>% 
-    #     dplyr::select(-...1)
-    #   
-    # }
+    states <- c(
+      "New South Wales",
+      "Victoria", 
+      "Queensland",
+      "South Australia",
+      "Western Australia",
+      "Tasmania",
+      "Northern Territory",
+      "Australian Capital Territory"
+      )
     
-
-    current_date <- hh_spending(fname, "Australia") %>%
-      dplyr::pull(date) %>%
-      max()
+    raw <- readabs::read_abs(cat_no = "5682.0", tables = 1:9, retain_files = FALSE) 
     
-  } else {
-    
-    current_date <- TRUE
-  }
-  
-  if (current_date > max(aitidata::household_spending$date) | force_update) {
-    
-    message("updating `household_spending`")
-    
-    states <- toupper(c("National", "NSW", "Vic", "Qld", "SA", "WA", "Tas", "NT", "ACT")) 
-    
-    cubes <- paste0( seq(1,9), "_", states)
-    
-    files <- purrr::map(.x = cubes, .f = ~aitidata::download_data_cube("monthly-household-spending-indicator", cube = .x, path = "data-raw"))
-    
-    household_spending <- purrr::map2_df(.x = files, .y =  states, .f = ~hh_spending(.x, .y))
+    household_spending <- raw %>%
+      readabs::separate_series(column_names = c("indicator", "coicop_division", "state", "price")) %>%
+      dplyr::filter(indicator == "Calendar adjusted household spending - Index") %>%
+      dplyr::mutate(value = ifelse(.data$unit == "000", 1000 * .data$value, .data$value),
+                    year = lubridate::year(.data$date),
+                    month = lubridate::month(.data$date, label = TRUE, abbr = FALSE)) %>%
+      dplyr::select("date", 
+                    "year", 
+                    "month",
+                    "indicator", 
+                    "coicop",
+                    "state",
+                    "series_type",
+                    "value",
+                    "unit")
     
     usethis::use_data(household_spending, overwrite = TRUE, compress = "xz")
+    return(TRUE)
     
-    any(file.remove(unlist(files)))
-     
   } else {
-    message("Skipping `household_spending,rda`: appears to be up-to-date")
-    return(fname)
+    
+    message("Monthly Household Spending does not need updating.")
   }
+  
 }
   
 

ff14a50c40ddf6a1090e9a6d166122fd9a6c87f1
Hamish Gamble <hamish.gamble@flinders.edu.au>
2023-07-24 15:37:59 +0930

    Incorporate labour account data for separate industries

--- R/update_labour_account.R
+++ R/update_labour_account.R
@@ -14,32 +14,36 @@ update_labour_account <- function(force_update = FALSE) {
   if (current_date > max(aitidata::labour_account$date) | force_update) {
     message("Updating `data/labour_account.rda`")
     
-    abs_file <- abs_test
+    purrr::walk(.x = sprintf("%0.2d", 2:20), 
+                .f = ~ aitidata::download_data_cube("labour-account-australia", 
+                                                    glue::glue("6150055003DO0{.x}.xlsx"),
+                                                    path = "data-raw/labour-account"))
     
-    labour_account <- readabs::read_abs_local(filenames = abs_file, path = "data-raw") %>%
+    labour_account <- readabs::read_abs_local(path = "data-raw/labour-account") %>%
       dplyr::mutate(series = ifelse((grepl("Public sector", .data$series) | grepl("Private sector", .data$series)), 
                                     gsub(x = .data$series, pattern = "; P", replacement = "- P"), 
                                     .data$series)) %>%
       tidyr::separate(.data$series, into = c("prefix", "indicator", "state", "industry"), sep = ";", extra = "drop") %>%
       dplyr::mutate(dplyr::across(where(is.character), trimws),
+                    industry = gsub("(.\\([A-S]\\))", x = industry, replacement = ""),
                     year = lubridate::year(.data$date),
                     month = lubridate::month(.data$date, abbr = FALSE, label = TRUE)) %>%
       dplyr::filter(!grepl(" - Percentage changes", .data$indicator),
                     !is.na(.data$value)) %>%
-      dplyr::select(.data$date, 
-                    .data$month, 
-                    .data$year, 
-                    .data$prefix, 
-                    .data$indicator, 
-                    .data$state, 
-                    .data$industry, 
-                    .data$series_type, 
-                    .data$value,
-                    .data$unit)
+      dplyr::select("date", 
+                    "month", 
+                    "year", 
+                    "prefix", 
+                    "indicator", 
+                    "state", 
+                    "industry", 
+                    "series_type", 
+                    "value",
+                    "unit")
     
     
     usethis::use_data(labour_account, overwrite = TRUE, compress = "xz")
-    file.remove(abs_file)
+    any(file.remove(paste0("data-raw/labour-account/",list.files("data-raw/labour-account"))))
 
   } else {
     message("Skipping `labour_account.rda`: appears to be up-to-date")

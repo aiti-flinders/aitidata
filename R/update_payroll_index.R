d97fdaae4d722058d74731626dd9f43efed57ed6
Hamish Gamble <hamish.gamble@flinders.edu.au>
2023-08-11 11:15:58 +0930

    Wage indexes have been permanently withdrawn from the payroll data

--- R/update_payroll_index.R
+++ R/update_payroll_index.R
@@ -40,35 +40,35 @@ update_payroll_index <- function(force_update = FALSE) {
     
     
     
-    payroll_wages <- payroll_index <- readxl::read_excel("data-raw/6160055001_DO004.xlsx", sheet = "Total wages index", col_types = "text", skip = 5) %>%
-      dplyr::rename_with(.fn = ~ dplyr::case_when(
-        .x == "State or Territory" ~ "state",
-        .x == "Industry division" ~ "industry",
-        .x == "Sub-division" ~ "industry_subdivision",
-        .x == "Employment size" ~ "emp_size",
-        .x == "Sex" ~ "gender",
-        .x == "Age group" ~ "age",
-        .x == "Statistical Area 4" ~ "sa4",
-        .x == "Statistical Area 3" ~ "sa3",
-        TRUE ~ to_snake(.x)
-      )) %>%
-      tidyr::pivot_longer(
-        cols = dplyr::starts_with("4"),
-        names_to = "date",
-        values_to = "value"
-      ) %>%
-      dplyr::mutate(value = suppressWarnings(as.numeric(.data$value))) %>%
-      dplyr::filter(.data$value != "NA") %>%
-      dplyr::mutate(date = as.Date(as.numeric(.data$date), origin = "1899-12-30"),
-                    dplyr::across(where(is.character), ~gsub(pattern = ".*\\. ", x =  .x, replacement = "", perl = TRUE)),
-                    indicator = "Payroll wages", 
-                    state = strayr::clean_state(.data$state, to = "state_name"))
+    # payroll_wages <- payroll_index <- readxl::read_excel("data-raw/6160055001_DO004.xlsx", sheet = "Total wages index", col_types = "text", skip = 5) %>%
+    #   dplyr::rename_with(.fn = ~ dplyr::case_when(
+    #     .x == "State or Territory" ~ "state",
+    #     .x == "Industry division" ~ "industry",
+    #     .x == "Sub-division" ~ "industry_subdivision",
+    #     .x == "Employment size" ~ "emp_size",
+    #     .x == "Sex" ~ "gender",
+    #     .x == "Age group" ~ "age",
+    #     .x == "Statistical Area 4" ~ "sa4",
+    #     .x == "Statistical Area 3" ~ "sa3",
+    #     TRUE ~ to_snake(.x)
+    #   )) %>%
+    #   tidyr::pivot_longer(
+    #     cols = dplyr::starts_with("4"),
+    #     names_to = "date",
+    #     values_to = "value"
+    #   ) %>%
+    #   dplyr::mutate(value = suppressWarnings(as.numeric(.data$value))) %>%
+    #   dplyr::filter(.data$value != "NA") %>%
+    #   dplyr::mutate(date = as.Date(as.numeric(.data$date), origin = "1899-12-30"),
+    #                 dplyr::across(where(is.character), ~gsub(pattern = ".*\\. ", x =  .x, replacement = "", perl = TRUE)),
+    #                 indicator = "Payroll wages", 
+    #                 state = strayr::clean_state(.data$state, to = "state_name"))
     
     
     
     
     
-    payroll_index <- dplyr::bind_rows(payroll_jobs, payroll_wages) %>%
+    payroll_index <- payroll_jobs %>%
       dplyr::mutate(industry = strayr::clean_anzsic(.data$industry, silent = TRUE),
                     industry = ifelse(is.na(.data$industry), "Total (industry)", .data$industry),
                     age = ifelse(.data$age == "All ages", "Total (age)", .data$age),

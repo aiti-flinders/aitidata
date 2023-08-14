c5efb773ceacb67b6dc190132ae229cc154912b6
Hamish Gamble <hamish.gamble@flinders.edu.au>
2023-08-14 13:38:24 +0930

    Removes some datasets from the update schedule

--- R/update_abs.R
+++ R/update_abs.R
@@ -16,12 +16,12 @@ update_abs <- function(force_update = FALSE) {
   update_national_accounts(force_update)
   update_underutilisation(force_update)
   update_labour_account(force_update)
-  update_jobseeker_sa2(force_update)
+  #update_jobseeker_sa2(force_update) jobseeker data is now being reported differently
   update_labour_force(force_update)
   update_retail_trade(force_update)
   update_cabee_sa2(force_update)
   update_payroll_index(force_update)
-  update_payroll_substate(force_update)
+  #update_payroll_substate(force_update) this has been removed from the ABS
   update_household_spending(force_update)
   update_covid_data() #this has to be after jobseeker, payroll, small area
 }

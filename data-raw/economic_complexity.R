2af94217db66e2f3407cbcdf342565cf64385873
Hamish Gamble <hamish.gamble@flinders.edu.au>
2024-01-12 10:44:11 +1030

    These data sets are currently not being updated

--- data-raw/economic_complexity.R
+++ data-raw/economic_complexity.R
@@ -1,16 +1,16 @@
-## code to prepare `economic_complexity` dataset goes here
-library(ecomplexity)
-library(tidyverse)
-
-states <- str_to_upper(strayr::strayr(seq(1, 8, 1)))
-
-economic_complexity <- get_data_states() %>%
-  filter(location_code %in% states) %>%
-  group_by(location_code, year) %>%
-  mutate(total_exports = sum(export_value)) %>%
-  ungroup() %>%
-  distinct(year, location_code, eci, coi, diversity, total_exports) %>%
-  pivot_longer(cols = c(3:6), names_to = "indicator", values_to = "value")
-
-
-usethis::use_data(economic_complexity, overwrite = TRUE, compress = "xz")
+# ## code to prepare `economic_complexity` dataset goes here
+# library(ecomplexity)
+# library(tidyverse)
+# 
+# states <- str_to_upper(strayr::strayr(seq(1, 8, 1)))
+# 
+# economic_complexity <- get_data_states() %>%
+#   filter(location_code %in% states) %>%
+#   group_by(location_code, year) %>%
+#   mutate(total_exports = sum(export_value)) %>%
+#   ungroup() %>%
+#   distinct(year, location_code, eci, coi, diversity, total_exports) %>%
+#   pivot_longer(cols = c(3:6), names_to = "indicator", values_to = "value")
+# 
+# 
+# usethis::use_data(economic_complexity, overwrite = TRUE, compress = "xz")
--- data-raw/industry_capability.R
+++ data-raw/industry_capability.R
@@ -1,26 +1,26 @@
-## code to prepare `industry_capability` dataset goes here
-library(tidyverse)
-library(aitidata)
-library(reticulate)
-
-sa2_zero_businesses <- cabee_sa2 %>%
-  group_by(sa2_name_2016) %>%
-  summarise(total = sum(total)) %>%
-  ungroup() %>%
-  filter(total == 0) %>%
-  pull(sa2_name_2016)
-
-
-input_data <- cabee_sa2 %>%
-  select(industry = division, sa2_name_2016, date, total) %>%
-  filter(industry != "Currently Unknown") %>% 
-  mutate(date = lubridate::year(date)) %>%
-  replace_na(replace = list(total = 0))
-
-ecomplexity <- import("ecomplexity")
-cols_input <- dict(time = "date", loc = "sa2_name_2016", prod = "industry", val = "total")
-out <- ecomplexity$ecomplexity(input_data, cols_input) %>%
-  as_tibble() %>%
-  filter(!is.nan(pci))
-
-usethis::use_data(industry_capability, overwrite = TRUE)
+# ## code to prepare `industry_capability` dataset goes here
+# library(tidyverse)
+# library(aitidata)
+# library(reticulate)
+# 
+# sa2_zero_businesses <- cabee_sa2 %>%
+#   group_by(sa2_name_2016) %>%
+#   summarise(total = sum(total)) %>%
+#   ungroup() %>%
+#   filter(total == 0) %>%
+#   pull(sa2_name_2016)
+# 
+# 
+# input_data <- cabee_sa2 %>%
+#   select(industry = division, sa2_name_2016, date, total) %>%
+#   filter(industry != "Currently Unknown") %>% 
+#   mutate(date = lubridate::year(date)) %>%
+#   replace_na(replace = list(total = 0))
+# 
+# ecomplexity <- import("ecomplexity")
+# cols_input <- dict(time = "date", loc = "sa2_name_2016", prod = "industry", val = "total")
+# out <- ecomplexity$ecomplexity(input_data, cols_input) %>%
+#   as_tibble() %>%
+#   filter(!is.nan(pci))
+# 
+# usethis::use_data(industry_capability, overwrite = TRUE)

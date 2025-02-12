7762eb323c19ea0827dda0962470cceeff434904
Hamish Gamble <hamish.gamble@flinders.edu.au>
2025-02-13 10:26:30 +1030

    Uses a more robust method for determing the date of each cabee sheet

--- R/update_cabee_sa2.R
+++ R/update_cabee_sa2.R
@@ -7,7 +7,6 @@
 
 update_cabee_sa2 <- function(force_update = FALSE) {
   
-  # The ABS is currently updating the CABEE release schedule. Add the check back after december 16th
   
   abs_test <- aitidata::download_data_cube(catalogue_string = "counts-australian-businesses-including-entries-and-exits",
                                            cube = 1,
@@ -21,13 +20,12 @@ update_cabee_sa2 <- function(force_update = FALSE) {
                   release = as.Date(paste0(.data$release, "-01"), format = "%B %Y-%d")) %>%
     dplyr::pull(.data$release)
   
-  #current_date <- TRUE
   
   if (current_date > max(aitidata::cabee_sa2$date) | force_update) {
     
     abs_file <- try(readabs::download_abs_data_cube(catalogue_string = "counts-australian-businesses-including-entries-and-exits", 
-                                                 cube = "8165DC08",
-                                                 path = "data-raw"), silent = TRUE)
+                                                    cube = "8165DC08",
+                                                    path = "data-raw"), silent = TRUE)
     
     if (!grepl("Error", abs_file)) {
       
@@ -36,6 +34,9 @@ update_cabee_sa2 <- function(force_update = FALSE) {
       cabee_sheets <- cabee_sheets[!is.na(cabee_sheets)]
       cabee_sheets <- cabee_sheets[stringr::str_detect(cabee_sheets, "b", negate = TRUE)]
       cabee_sheets <- paste("Table", cabee_sheets)
+      
+      # SA2 Business Counts include both point in time (odd # sheets) and annualised employment size ranges (even # sheets)
+      cabee_sheets <- cabee_sheets[grepl("[246]", cabee_sheets)]
       
       cabee_sa2 <- tibble::tribble(
         ~"date",
@@ -51,28 +52,37 @@ update_cabee_sa2 <- function(force_update = FALSE) {
         ~"total"
       )
       
-      for (i in cabee_sheets) {
-        cabee_year <- readxl::read_excel(abs_file,
-                                         sheet = i,
-                                         skip = 7,
-                                         col_names = c(
-                                           "industry_code",
-                                           "industry_label",
-                                           "sa2_main_2016",
-                                           "sa2_name_2016",
-                                           "non_employing",
-                                           "employing_1_4",
-                                           "employing_5_19",
-                                           "employing_20_199",
-                                           "employing_200_plus",
-                                           "total"
-                                         )) %>%
-          dplyr::filter(!is.na(.data$sa2_main_2016)) %>%
-          dplyr::mutate(sa2_main_2016 = as.character(.data$sa2_main_2016),
-                        date = as.Date(paste(2024 -  as.numeric(stringr::str_extract(i, "\\d+")), "06", "01"), format = "%Y %M %d"))
-        
-        cabee_sa2 <- dplyr::bind_rows(cabee_year, cabee_sa2)
-      }
+      cabee_years <- purrr::map(.x = cabee_sheets,
+                                .f = ~readxl::read_excel(abs_file,
+                                                         sheet = .x,
+                                                         col_names = "year",
+                                                         range = "A4") |> 
+                                  dplyr::mutate(year = as.Date(paste0("01", stringr::str_extract_all(year, "June \\d+")), "%d %B %Y")) |> 
+                                  dplyr::pull(year)) |> 
+        purrr::list_c()
+      
+      cabee_sa2 <- purrr::map2(.x = cabee_sheets,
+                               .y = cabee_years,
+                               .f = ~ readxl::read_excel(abs_file,
+                                                         sheet = i,
+                                                         skip = 7,
+                                                         col_names = c(
+                                                           "industry_code",
+                                                           "industry_label",
+                                                           "sa2_main_2016",
+                                                           "sa2_name_2016",
+                                                           "non_employing",
+                                                           "employing_1_4",
+                                                           "employing_5_19",
+                                                           "employing_20_199",
+                                                           "employing_200_plus",
+                                                           "total"
+                                                         )) |> 
+                                 dplyr::filter(!is.na(.data$sa2_main_2016)) |> 
+                                 dplyr::mutate(sa2_main_2016 = as.character(.data$sa2_main_2016),
+                                               date = .y)) |> 
+        purrr::list_rbind()
+      
       
       cabee_sa2 <- cabee_sa2 %>%
         tidyr::pivot_longer(cols = -c("industry_code", "industry_label", "sa2_main_2016", "sa2_name_2016", "date"),

6311e82d7b481d47e2119fa76530ab0e302c05b0
Hamish Gamble <hamish.gamble@flinders.edu.au>
2025-03-17 11:13:52 +1030

    Exclude update_abs from files to update

--- data-raw/update_abs.R
+++ data-raw/update_abs.R
@@ -1,5 +1,7 @@
 source_files <- list.files("data-raw")
 source_files <- paste0("data-raw/", source_files[grep(".R", source_files)])
+#Exclude THIS file from being sourced - or else get stuck in a loop
+source_files <- source_files[!grepl("data-raw/update_abs.R", source_files)]
 Map(source, source_files)
 devtools::document()
 

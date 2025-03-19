deda4f82f4cb61e7d82df032829dad8f3a38ea64
Hamish Gamble <hamish.gamble@flinders.edu.au>
2025-03-19 13:20:39 +1030

    Separate payroll data files

--- data-raw/payroll_data.R
+++ data-raw/payroll_data.R
@@ -1,15 +1,17 @@
 ## code to prepare `payroll_data` dataset goes here
 library(readabs)
+library(usethis)
 library(purrr)
 
-payroll_index <- map(
-  .x = c("industry_jobs", 
-         "subindustry_jobs", 
-         "empsize_jobs"), 
-  .f = function(x) read_payrolls(x, path = "data-raw"))
+payroll_index <- read_payrolls("industry_jobs", path = "data_raw")
+payroll_index_industry <- read_payrolls("subindustry_jobs", path = "data-raw")
+payroll_index_business <- read_payrolls("empsize_jobs", path = "data-raw")
+
 
 file.remove(paste0("data-raw/", c("6160055001_DO001.xlsx",
                                   "6160055001_DO002.xlsx",
                                   "6160055001_DO003.xlsx")))
 
 use_data(payroll_index, overwrite = TRUE, compress = "xz")
+use_data(payroll_index_industry, overwrite = TRUE, compress = "xz")
+use_data(payroll_index_business, overwrite = TRUE, compress = "xz")

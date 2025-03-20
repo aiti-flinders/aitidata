d0b4cc494592cc9b361c754845a1c0482e8c4695
Hamish Gamble <hamish.gamble@flinders.edu.au>
2025-03-20 11:34:57 +1030

    These files aren't downloaded so don't need to be removed

--- data-raw/payroll_data.R
+++ data-raw/payroll_data.R
@@ -7,11 +7,6 @@ payroll_index <- read_payrolls("industry_jobs", path = "data_raw")
 payroll_index_industry <- read_payrolls("subindustry_jobs", path = "data-raw")
 payroll_index_business <- read_payrolls("empsize_jobs", path = "data-raw")
 
-
-file.remove(paste0("data-raw/", c("6160055001_DO001.xlsx",
-                                  "6160055001_DO002.xlsx",
-                                  "6160055001_DO003.xlsx")))
-
 use_data(payroll_index, overwrite = TRUE, compress = "xz")
 use_data(payroll_index_industry, overwrite = TRUE, compress = "xz")
 use_data(payroll_index_business, overwrite = TRUE, compress = "xz")
